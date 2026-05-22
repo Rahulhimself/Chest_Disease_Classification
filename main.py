e065a8d109072656d4dbbd3129f1b1e32c9fc5c4
rahulbansode <rahulbkumar118@gmail.com>
2026-05-23 02:14:11 +0530

    stage 03 training base model file added/completed

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+import os
+#Suppress TensorFlow's internal oneDNN optimization warnings
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+
+#Suppress standard TensorFlow logging (Change '2' to '3' to hide everything except fatal errors)
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' 
+
+
 from src.cnnClassifier import logger
 from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-
+from src.cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from src.cnnClassifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -13,3 +22,25 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
